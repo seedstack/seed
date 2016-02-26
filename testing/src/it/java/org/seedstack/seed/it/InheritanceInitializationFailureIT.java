e25d59a43456987c5e17099fbb5866687df9593c
Adrien LAUER <adrien.lauer@gmail.com>
2016-02-26 19:18:10 +0100

    Add missing IT

---
+++ testing/src/it/java/org/seedstack/seed/it/InheritanceInitializationFailureIT.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2013-2016, The SeedStack authors <http://seedstack.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.seedstack.seed.it;
+
+import org.junit.Test;
+
+import javax.inject.Inject;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Expect(RuntimeException.class)
+public class InheritanceInitializationFailureIT extends AbstractSeedIT {
+    @Inject
+    Object object;
+
+    @Test
+    public void injection_should_not_work() {
+        assertThat(object).isNull();
+    }
+}
