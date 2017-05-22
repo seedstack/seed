640c52a1ec5cd1540e31cad9c5f9a62ea1c2b9e5
Adrien LAUER <adrien.lauer@mpsa.com>
2017-05-22 12:30:58 +0200

    Add config mapping of Guava HostAndPort

---
+++ core/src/main/java/org/seedstack/seed/core/internal/configuration/HostAndPortMapper.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2013-2016, The SeedStack authors <http://seedstack.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.seedstack.seed.core.internal.configuration;
+
+import com.google.common.net.HostAndPort;
+import org.seedstack.coffig.TreeNode;
+import org.seedstack.coffig.node.ValueNode;
+import org.seedstack.coffig.spi.ConfigurationMapper;
+
+import java.lang.reflect.Type;
+
+public class HostAndPortMapper implements ConfigurationMapper {
+    @Override
+    public boolean canHandle(Type type) {
+        return type instanceof Class && HostAndPort.class.isAssignableFrom((Class<?>) type);
+    }
+
+    @Override
+    public Object map(TreeNode treeNode, Type type) {
+        return HostAndPort.fromString(treeNode.value());
+    }
+
+    @Override
+    public TreeNode unmap(Object object, Type type) {
+        return new ValueNode(object.toString());
+    }
+}
