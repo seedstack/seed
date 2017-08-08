5d13e87034408e871c7f15b9ed8c5c89f920ddb0
Adrien LAUER <adrien.lauer@mpsa.com>
2017-08-08 15:44:08 +0200

    Configuration dump now displays inner properties

--- core/src/main/java/org/seedstack/seed/core/internal/configuration/tool/ConfigTool.java
+++ core/src/main/java/org/seedstack/seed/core/internal/configuration/tool/ConfigTool.java
@@ -10,9 +10,11 @@ package org.seedstack.seed.core.internal.configuration.tool;
 import io.nuun.kernel.api.plugin.InitState;
 import io.nuun.kernel.api.plugin.context.InitContext;
 import io.nuun.kernel.api.plugin.request.ClasspathScanRequest;
+import org.seedstack.coffig.Coffig;
 import org.seedstack.coffig.Config;
 import org.seedstack.seed.SeedException;
 import org.seedstack.seed.cli.CliArgs;
+import org.seedstack.seed.core.SeedRuntime;
 import org.seedstack.seed.core.internal.AbstractSeedTool;
 import org.seedstack.seed.core.internal.CoreErrorCode;
 
@@ -26,10 +28,16 @@ public class ConfigTool extends AbstractSeedTool {
     private final Node root = new Node();
     @CliArgs
     private String[] args;
+    private Coffig configuration;
 
     @Override
     public String toolName() {
         return "config";
+    }
+
+    @Override
+    protected void setup(SeedRuntime seedRuntime) {
+        configuration = seedRuntime.getConfiguration();
     }
 
     @Override
@@ -42,7 +50,7 @@ public class ConfigTool extends AbstractSeedTool {
     @Override
     protected InitState initialize(InitContext initContext) {
         List<Node> nodes = new ArrayList<>();
-        initContext.scannedClassesByAnnotationClass().get(Config.class).stream().map(Node::new).forEach(nodes::add);
+        initContext.scannedClassesByAnnotationClass().get(Config.class).stream().map(configClass -> new Node(configClass, configuration)).forEach(nodes::add);
         Collections.sort(nodes);
         nodes.forEach(this::buildTree);
         return InitState.INITIALIZED;
@@ -67,7 +75,7 @@ public class ConfigTool extends AbstractSeedTool {
     private void info(String[] path) {
         Node node = root.find(Arrays.copyOfRange(path, 0, path.length - 1));
         if (node == null) {
-            throw SeedException.createNew(CoreErrorCode.INVALID_CONFIG_PATH).put("path", path);
+            throw SeedException.createNew(CoreErrorCode.INVALID_CONFIG_PATH).put("path", String.join(".", path));
         } else {
             PropertyInfo propertyInfo = node.getPropertyInfo(path[path.length - 1]);
             if (propertyInfo == null) {
