69bd7f59be8d142ac90706d1dd61da9fc8e23fc2
Adrien LAUER <adrien.lauer@gmail.com>
2021-04-14 17:48:17 +0200

    Update guava to 30.1.1, poms to 4.0.0-S

--- cli/src/main/java/org/seedstack/seed/cli/internal/WithCliCommandResolver.java
+++ cli/src/main/java/org/seedstack/seed/cli/internal/WithCliCommandResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2013-2020, The SeedStack authors <http://seedstack.org>
+ * Copyright © 2013-2021, The SeedStack authors <http://seedstack.org>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
